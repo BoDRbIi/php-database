1012025f86f522c2e01ccd616ac6fb0f584156d9
gostkov <gostkov.evg@gmail.com>
2012-04-14 02:38:22 +0600

    Дизайн доделал

---
+++ forum.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: evgeniy
+ * Date: 14.04.12
+ * Time: 1:45
+ * To change this template use File | Settings | File Templates.
+ */
--- index.php
+++ index.php
@@ -1,31 +1,45 @@
 <!DOCTYPE HTML>
 <html>
+    <!--
+        Design: Gostkov Evgeniy
+        date: 6.04.2012
+     -->
     <head>
-        <title>php-social-net</title>
+        <title>Main page | Database-project</title>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="./css/base.css" type="text/css">
     </head>
 
     <body>
         <!-- div header - include  -->
         <div id="main-header">
-
+            <a href="index.php">
+                <img src="images/header.png">
+            </a>
         </div>
-
-        <!-- div main content include all content(text, images, etc) -->
+        <hr />
         <div id="main-content">
-            <ul>
-                <li>Main page</li>
-                <li>Forum</li>
-                <li>News</li>
-                <li>Photo gallery</li>
-                <li>Social-net</li>
-            </ul>
-            
+            <!-- menu of site -->
+            <div class="menu">
+                <ul>
+                    <li><a href="index.php">Main page</a></li>
+                    <li><a href="forum.php">Forum</a></li>
+                    <li><a href="news.php">News</a></li>
+                    <li><a href="gallery.php">Photo gallery</a></li>
+                    <li><a href="social.php">Social-net</a></li>
+                    <li><a href="author.html">Authors</a></li>
+                </ul>
+            </div>
+
+            <!-- other content -->
+            <div id="content">
+                Database. Third task. web-site with support forum, gallery, social network
+            </div>
         </div><!-- end main content-->
 
         <!--footer include copyright information and etc... -->
         <div id="main-footer">
-
-
+            <a href="author.html">&copy; Authors page</a>
         </div>
         <!-- end div copyrigth-->
     </body>
