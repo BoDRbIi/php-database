1a0649f33b27e0d35af9f76d89218c5ea4c619c1
gostkov <gostkov.evg@gmail.com>
2012-04-14 00:23:02 +0600

    add structure

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>php-social-net</title>
+    </head>
+
+    <body>
+        <!-- div header - include  -->
+        <div id="main-header">
+
+        </div>
+
+        <!-- div main content include all content(text, images, etc) -->
+        <div id="main-content">
+            <ul>
+                <li>Main page</li>
+                <li>Forum</li>
+                <li>News</li>
+                <li>Photo gallery</li>
+                <li>Social-net</li>
+            </ul>
+            
+        </div><!-- end main content-->
+
+        <!--footer include copyright information and etc... -->
+        <div id="main-footer">
+
+
+        </div>
+        <!-- end div copyrigth-->
+    </body>
+</html>
 <?php
 
 ?>
